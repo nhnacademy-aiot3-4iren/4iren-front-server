2ab513a740d4bb64cc2587a75076b82e89cac560
jaena9958-art <jaena9958@gmail.com>
2026-08-18 17:07:59 +0900

    feat : lading page 완성
    작성되지 않은 부분이나 수정하지 않은 부분 추가, 수정 완료
    
    Fixed #25

--- src/main/java/com/nhnacademy/front/controller/WebController.java
+++ src/main/java/com/nhnacademy/front/controller/WebController.java
@@ -41,4 +41,7 @@ public class WebController {
     @GetMapping("/team")
     public String getMyTeams() { return "/mypage/myteams"; }
 
+    @GetMapping("/alert-history")
+    public String getAlertHistory() { return "/alarms/alert-history"; }
+
 }
